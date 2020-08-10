2b83ae26c93639f8d0a55fd6671dbf6a4e001518
Adegunwa Toluwalope <Adegunwatoluwalope@gmail.com>
2020-08-10 12:27:52 +0100

    initial commit

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,8 +1,35 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from .models import User, Education, Skill, Social, \
     ProjectImage, Project, SocialLink, Experience, SubSkill
 
-admin.site.register(User)
+class ProjectInline(admin.TabularInline):
+    model = Project
+    extra = 1
+
+class ExperienceInline(admin.TabularInline):
+    model = Experience
+    extra = 1
+
+class EducationInline(admin.TabularInline):
+    model = Education
+    extra = 1
+
+class SkillInline(admin.TabularInline):
+    model = Skill
+    extra = 1
+
+class SocialLinkInline(admin.TabularInline):
+    model = Project
+    extra = 1
+
+
+class UserAdmin(BaseUserAdmin):
+    inlines = (ProjectInline, ExperienceInline, EducationInline, SkillInline, SocialLinkInline)
+
+
+
+admin.site.register(User, UserAdmin)
 admin.site.register(Education)
 admin.site.register(Skill)
 admin.site.register(Social)
