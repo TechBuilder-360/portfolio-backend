927050ef5b20cddcb4896a0444ee859141ac9602
Adegunwa Toluwalope <Adegunwatoluwalope@gmail.com>
2020-09-08 21:16:45 +0100

    set allowed hosts

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -9,9 +9,9 @@ from graphene_django.views import GraphQLView
 urlpatterns = [
     path('admin/', admin.site.urls),
     # path('accounts/', include('django.contrib.auth.urls')),
-    path('accounts/', include('accounts.urls')),
+    # path('accounts/', include('accounts.urls')),
     path("graphql/", csrf_exempt(GraphQLView.as_view(graphiql=True))),
-    path('', TemplateView.as_view(template_name="accounts/google_auth.html")),
-    path('', include('social_django.urls', namespace='social')),
-    path('logout/', login_views.LogoutView.as_view(), {'next_page': settings.LOGOUT_REDIRECT_URL}, name='logout'),
+    path('', TemplateView.as_view(template_name="accounts/base.html")),
+    # path('', include('social_django.urls', namespace='social')),
+    # path('logout/', login_views.LogoutView.as_view(), {'next_page': settings.LOGOUT_REDIRECT_URL}, name='logout'),
 ]
